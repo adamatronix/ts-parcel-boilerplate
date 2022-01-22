ce46cf1fa35fb4fac8b6281e88fda95e5e3f5a51
Your Name <you@example.com>
2022-01-22 10:07:33 +0800

    tsc-compile Add all sorts of updates including tsc for compilation

---
+++ src/Examples.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const Example = () => {
+  return (
+    <></>
+  )
+  
+}
+
+render(<Example/>,
+  document.getElementById('root')
+);
+
--- src/example.ts
+++
@@ -1,7 +0,0 @@
-import Example from "./Example/Example"; 
-
-window.onload = () => {
-
-  new Example();
-
-}
